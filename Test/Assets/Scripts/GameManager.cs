c0c1c89d329d363fc473fa5a6e62a63aa9b3e1a7
Statys <vsastatys@gmail.com>
2017-05-27 04:06:38 +0500

    First lesson. 26 minutes

---
+++ Test/Assets/Scripts/GameManager.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+    public GameObject tilePrefab;
+    public GameObject userPlayerPrefab;
+
+    public int MapSizeX = 10;
+    public int MapSizeY = 10;
+    public int CurrentPlayerIndex = 0;
+
+    List<List<Tile>> map = new List<List<Tile>>();
+    List<Player> listPlayers = new List<Player>();
+
+	// Use this for initialization
+	void Start () {
+        GenerateMap();
+        GeneratePlayers();
+	}
+
+    private void GeneratePlayers()
+    {
+        listPlayers.Clear();
+
+        UserPlayer player = ((GameObject)(Instantiate(userPlayerPrefab, new Vector3(- Mathf.Floor(MapSizeX / 2), 1.5f, Mathf.Floor(MapSizeY / 2)), Quaternion.Euler(new Vector3())))).GetComponent<UserPlayer>();
+
+        listPlayers.Add(player);
+
+        player = ((GameObject)(Instantiate(userPlayerPrefab, new Vector3(MapSizeX-1-Mathf.Floor(MapSizeX / 2), 1.5f, -MapSizeY+1 + Mathf.Floor(MapSizeY / 2)), Quaternion.Euler(new Vector3())))).GetComponent<UserPlayer>();
+
+        listPlayers.Add(player);
+    }
+
+    private void GenerateMap()
+    {
+        for (int i = 0; i < MapSizeX; i++)
+        {
+            List<Tile> row = new List<Tile>();
+            for (int j = 0; j < MapSizeY; j++)
+            {
+                Tile tile = ((GameObject)(Instantiate(tilePrefab, new Vector3(i - Mathf.Floor(MapSizeX/2),0,-j+Mathf.Floor(MapSizeY/2)), Quaternion.Euler(new Vector3())))).GetComponent<Tile>();
+                tile.GridPosition = new Vector2(i, j);
+                row.Add(tile);
+            }
+            map.Add(row);
+        }
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
---
+++ Test/Assets/Scripts/Tile.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tile : MonoBehaviour {
+
+    public Vector2 GridPosition = Vector2.zero;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnMouseEnter()
+    {
+        GetComponent<Renderer>().material.color = Color.blue;
+
+        Debug.Log(string.Format("My position is x:{0} y:{1}",GridPosition.x, GridPosition.y));
+    }
+
+    void OnMouseExit()
+    {
+        GetComponent<Renderer>().material.color = Color.white;
+    }
+}
